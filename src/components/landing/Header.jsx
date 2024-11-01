869ef51b97874eb983c01ce8576aaf19be18ff28
Ziyardeen Alhassan <zforkah@gmail.com>
2024-11-01 17:45:16 +0000

    Update

--- src/components/landing/Header.jsx
+++ src/components/landing/Header.jsx
@@ -1,59 +1,57 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
-import { useLocation } from 'react-router-dom';
-
-
-
+import { Link, useLocation } from 'react-router-dom';
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const location = useLocation();
 
-  const [isOpen, setIsOpen] =useState(false)
-  const location = useLocation()
- 
+  const isHomePage = location.pathname === '/';
 
-  const isHomePage = location.pathname === '/'
-
-
-  const toggleMenu = ()=>{
-    setIsOpen(!isOpen)
-    
-  }
-    return (
-      <header className=" text-white p-4 -image bg-cover bg-primary">
-
-        <div className="container mx-auto flex justify-between items-center ">
-          <h1 className="text-2xl font-bold" aria-label="Community Events Logo"><a href="/">Occasionton</a></h1>
-          <button className='md:hidden' onClick={toggleMenu}>{isOpen?<FaTimes/>:<FaBars/>}</button>
-
-
-          <nav className={` hidden md:flex` }>
-            <ul className={`flex space-x-4`}>
-              {isHomePage && <>
-                  <li><a href="#features" className="hover:underline">Features</a></li>
-                  <li><a href="#how-it-works" className="hover:underline">How It Works</a></li>
-                  <li><a href="#contact" className="hover:underline">Contact Us</a></li>
-              </>
-              }
-              <li><a href="/signin" className="hover:underline">Login</a></li>
-              <li><a href="/signup" className="hover:underline">Sign Up</a></li>
-            </ul>
-          </nav>
-        </div>
-
-        <nav className={`${isOpen?'flex justify-center':'hidden'} md:hidden` }>
-        <ul className={`flex space-x-4`}>
-              {isHomePage && <>
-                  <li><a href="#features" className="hover:underline">Features</a></li>
-                  <li><a href="#how-it-works" className="hover:underline">How It Works</a></li>
-                  <li><a href="#contact" className="hover:underline">Contact Us</a></li>
-              </>
-              }
-              <li><a href="/signin" className="hover:underline">Login</a></li>
-              <li><a href="/signup" className="hover:underline">Sign Up</a></li>
-            </ul>
-          </nav>
-      </header>
-    );
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
   };
 
-export default Header
+  return (
+    <header className="text-white p-4 bg-primary">
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="text-2xl font-bold" aria-label="Community Events Logo">
+          <Link to="/">Occasionton</Link>
+        </h1>
+        <button className="md:hidden" onClick={toggleMenu}>
+          {isOpen ? <FaTimes /> : <FaBars />}
+        </button>
+
+        <nav className={`hidden md:flex`}>
+          <ul className="flex space-x-4">
+            {isHomePage && (
+              <>
+                <li><Link to="#features" className="hover:underline">Features</Link></li>
+                <li><Link to="#how-it-works" className="hover:underline">How It Works</Link></li>
+                <li><Link to="#contact" className="hover:underline">Contact Us</Link></li>
+              </>
+            )}
+            <li><Link to="/signin" className="hover:underline">Login</Link></li>
+            <li><Link to="/signup" className="hover:underline">Sign Up</Link></li>
+          </ul>
+        </nav>
+      </div>
+
+      <nav className={`${isOpen ? 'flex justify-center' : 'hidden'} md:hidden`}>
+        <ul className="flex space-x-4">
+          {isHomePage && (
+            <>
+              <li><Link to="#features" className="hover:underline">Features</Link></li>
+              <li><Link to="#how-it-works" className="hover:underline">How It Works</Link></li>
+              <li><Link to="#contact" className="hover:underline">Contact Us</Link></li>
+            </>
+          )}
+          <li><Link to="/signin" className="hover:underline">Login</Link></li>
+          <li><Link to="/signup" className="hover:underline">Sign Up</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
